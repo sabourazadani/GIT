c6604158fe03cfbcf1a78a8bbfbc2cb9be3f17f0
Sabour Azadani <sabour.azadani@gmail.com>
2022-08-24 19:06:55 -0500

    Updated Example.java

--- Example.java
+++ Example.java
@@ -1,1 +1,7 @@
 I have created Example.java class
+  
+// You have to create an automation method for Example.java
+  
+  public void Example (){
+    System.out.println("Printing the Example.java"); 
+  }
